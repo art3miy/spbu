0ff321e8e669867044d51d1b8c8d2dae6a0c8d83
art3m <art3m.shmagin@gmail.com>
2018-11-07 22:01:30 +0300

    added test file and auto-initialization of path with this test file

--- sem1/hw4/4.3.cpp
+++ sem1/hw4/4.3.cpp
@@ -2,9 +2,11 @@
 #include <fstream>
 using namespace std;
 
+const int maxStringSize = 260;
+
 void readPath(char* path)
 {
-    cin.getline(path, 260);
+    cin.getline(path, maxStringSize);
 }
 
 bool isEmpty(char* string)
@@ -23,24 +25,36 @@ bool isEmpty(char* string)
 
 int main()
 {
-    cout << "input path to your file\n";
-    char path[260] = {};
-    readPath(path);
-    cout << "here is path from your input\n";
-    cout << path << "\n";
+	char path[maxStringSize] = "test.txt";
+	cout << "Do you want to input path of the file by yourself y/n\n";
+	char answer = '\0';
+	cin >> answer;
+	if (answer != 'y' && answer != 'n')
+	{
+		cerr << "input the correct answer";
+		return 1;
+	}
+	else if (answer == 'y')
+	{
+        cout << "input path to your file\n";
+        cin.ignore();
+        readPath(path);
+        cout << "here is path from your input\n";
+        cout << path << "\n";
+    }
     ifstream file(path);
     if (!file)
     {
         cerr << "error reading file, input the correct path\n";
-        return -1;
+        return 2;
     }
     else
     {
-        char string[255] = {};
+        char string[maxStringSize] = {};
         int counter = 0;
         while (!file.eof())
         {
-            file.getline(string, 255);
+            file.getline(string, maxStringSize);
             if (!isEmpty(string))
             {
                 ++counter;
