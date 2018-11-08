4fd0090cfc68614f3e595c5cb93eda326f8d8dc7
art3m <art3m.shmagin@gmail.com>
2018-11-08 23:12:30 +0300

    added const values, remade a printStackReverse function, deleted some extra indents

--- sem1/hw5/5.3/main.cpp
+++ sem1/hw5/5.3/main.cpp
@@ -2,124 +2,122 @@
 #include "stack.h"
 using namespace std;
 
+const int maxExpressionLength = 260;
+
 void printExpression(char* expression, int length)
 {
-    for (int i = 0; i < length; ++i)
-    {
-        cout << expression[i] << " ";
-    }
-    cout << "\n";
+	for (int i = 0; i < length; ++i)
+	{
+		cout << expression[i] << " ";
+	}
+	cout << "\n";
 }
 
 void readExpression(char* expression)
 {
-    cin.getline(expression, 255);
+	cin.getline(expression, maxExpressionLength);
 }
 
 void printStackReverse(Stack* stack)
 {
-    if (!stack->head)
-    {
-        cout << "error : stack is empty\n";
-        return;
-    }
-    const int sizeOfStack = size(stack);
-    char* expression = new char[sizeOfStack];
-    int index = 0;
-    StackElement* current = stack->head;
-    while (current)
-    {
-        expression[sizeOfStack - index - 1] = current->value;
-        current = current->next;
-        ++index;
-    }
-    printExpression(expression, sizeOfStack);
-    delete [] expression;
-
+	if (!stack->head)
+	{
+		cout << "error : stack is empty\n";
+		return;
+	}
+	Stack* expression = createStack();
+	StackElement* current = stack->head;
+	while (current)
+	{
+		push(expression, current->value);
+		current = current->next;
+	}
+	printStack(expression);
+	deleteStack(expression);
 }
 
 void translateExpression(char* expression, Stack* result)
 {
-    Stack* operatorsStack = createStack();
-    for (int i = 0; expression[i] != '\0'; ++i)
-    {
-        if (expression[i] >= '0' && expression[i] <= '9')
-        {
-            push(result, expression[i]);
-        }
-        else if (expression[i] == '(')
-        {
-            push(operatorsStack, expression[i]);
-        }
-        else if (expression[i] == ')')
-        {
-            if (!operatorsStack->head)
-            {
-                cout << "error with brackets!\n";
-                return;
-            }
-            while (top(operatorsStack) != '(')
-            {
-                push(result, top(operatorsStack));
-                pop(operatorsStack);
-            }
-            pop(operatorsStack);
-        }
-        else if (expression[i] == '+' || expression[i] == '-')
-        {
-            if (!operatorsStack->head)
-            {
-                push(operatorsStack, expression[i]);
-            }
-            else if (top(operatorsStack) == '+' || top(operatorsStack) == '-' || top(operatorsStack) == '*' || top(operatorsStack) == '/')
-            {
-                push(result, top(operatorsStack));
-                pop(operatorsStack);
-                push(operatorsStack, expression[i]);
-            }
-            else
-            {
-                push(operatorsStack, expression[i]);
-            }
-        }
-        else if (expression[i] == '*' || expression[i] == '/')
-        {
-            if (!operatorsStack->head)
-            {
-                push(operatorsStack, expression[i]);
-            }
-            else if (top(operatorsStack) == '*' || top(operatorsStack) == '/')
-            {
-                push(result, top(operatorsStack));
-                pop(operatorsStack);
-                push(operatorsStack, expression[i]);
-            }
-            else if (top(operatorsStack) == '+' || top(operatorsStack) == '-')
-            {
-                push(operatorsStack, expression[i]);
-            }
-            else
-            {
-                push(operatorsStack, expression[i]);
-            }
-        }
-    }
-    while (operatorsStack->head)
-    {
-        push(result, top(operatorsStack));
-        pop(operatorsStack);
-    }
+	Stack* operatorsStack = createStack();
+	for (int i = 0; expression[i] != '\0'; ++i)
+	{
+		if (expression[i] >= '0' && expression[i] <= '9')
+		{
+			push(result, expression[i]);
+		}
+		else if (expression[i] == '(')
+		{
+			push(operatorsStack, expression[i]);
+		}
+		else if (expression[i] == ')')
+		{
+			if (!operatorsStack->head)
+			{
+				cout << "error with brackets!\n";
+				return;
+			}
+			while (top(operatorsStack) != '(')
+			{
+				push(result, top(operatorsStack));
+				pop(operatorsStack);
+			}
+			pop(operatorsStack);
+		}
+		else if (expression[i] == '+' || expression[i] == '-')
+		{
+			if (!operatorsStack->head)
+			{
+				push(operatorsStack, expression[i]);
+			}
+			else if (top(operatorsStack) == '+' || top(operatorsStack) == '-' || top(operatorsStack) == '*' || top(operatorsStack) == '/')
+			{
+				push(result, top(operatorsStack));
+				pop(operatorsStack);
+				push(operatorsStack, expression[i]);
+			}
+			else
+			{
+				push(operatorsStack, expression[i]);
+			}
+		}
+		else if (expression[i] == '*' || expression[i] == '/')
+		{
+			if (!operatorsStack->head)
+			{
+				push(operatorsStack, expression[i]);
+			}
+			else if (top(operatorsStack) == '*' || top(operatorsStack) == '/')
+			{
+				push(result, top(operatorsStack));
+				pop(operatorsStack);
+				push(operatorsStack, expression[i]);
+			}
+			else if (top(operatorsStack) == '+' || top(operatorsStack) == '-')
+			{
+				push(operatorsStack, expression[i]);
+			}
+			else
+			{
+				push(operatorsStack, expression[i]);
+			}
+		}
+	}
+	while (operatorsStack->head)
+	{
+		push(result, top(operatorsStack));
+		pop(operatorsStack);
+	}
 }
 
 int main()
 {
-    cout << "input your expression (you can only use digits, '(' and ')' and arithmetic operators '+' '-' '*' '/')\n";
-    char expression[255] = {};
-    Stack* result = createStack();
-    readExpression(expression);
-    translateExpression(expression, result);
-    cout << "here is your expression now\n";
-    printStackReverse(result);
-    deleteStack(result);
-    return 0;
+	cout << "input your expression (you can only use digits, '(' and ')' and arithmetic operators '+' '-' '*' '/')\n";
+	char expression[maxExpressionLength] = {};
+	Stack* result = createStack();
+	readExpression(expression);
+	translateExpression(expression, result);
+	cout << "here is your expression now\n";
+	printStackReverse(result);
+	deleteStack(result);
+	return 0;
 }
--- sem1/hw5/5.3/stack.h
+++ sem1/hw5/5.3/stack.h
@@ -2,25 +2,20 @@
 
 struct StackElement
 {
-    char value;
-    StackElement* next;
+	char value;
+	StackElement* next;
 };
 
 struct Stack
 {
-    StackElement* head;
+	StackElement* head;
 };
 
 Stack* createStack();
-
 void deleteStack(Stack* stack);
 
 void push(Stack* stack, char value);
-
 void pop(Stack* stack);
-
 void printStack(Stack* stack);
-
 char top(Stack* stack);
-
 int size(Stack* stack);
